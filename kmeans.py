d7931c0e566edcfb4f1e0b32f6ca461cd7069a88
PROLCY <ckd248@naver.com>
2024-05-09 22:29:00 +0900

    add preprocess

--- kmeans.py
+++ kmeans.py
@@ -4,38 +4,26 @@ import cv2
 import numpy as np
 from sklearn.cluster import KMeans
 
-from utils import convert_series_to_keypoints, norm_pose, write_cluster_centers, write_pose_data, write_cluster_cnt
+from utils import convert_series_to_keypoints, preprocess_keypoints_for_clustering, write_cluster_centers, write_pose_data, write_cluster_cnt, get_cluster_center_by_choice
 from vis_clusters import vis_clusters
 
 k_cluster = 30
 partition_size = (7, 2)
 random_state = 0
-tag = '_kmeans_temp'
+tag = '_kmeans_temp3'
 target_data_path = os.path.join('data', tag)
 if not os.path.exists(target_data_path):
     os.mkdir(target_data_path)
 
-
 data_path = './affordance_data/trainlist.txt'
-with open(data_path, 'r') as f:
-    data_list = list(f.readlines())
-data_list = [x.split(' ') for x in data_list]
-
-image_name_list = []
-keypoints_list = []
-norm_keypoints_list = []
-for data in data_list:
-    image_name = data[0]
-    keypoint = data[1:-3]
-    keypoint = [eval(x) for x in keypoint]
-    image_name_list.append(image_name)
-    keypoints_list.append(keypoint)
-    norm_keypoints_list.append(norm_pose(keypoint))
-
-kmeans = KMeans(n_clusters=k_cluster, random_state=random_state, n_init="auto").fit(np.array(norm_keypoints_list))
+image_name_list, keypoints_list, norm_keypoints_list = preprocess_keypoints_for_clustering(data_path)
+kmeans = KMeans(n_clusters=k_cluster, random_state=random_state, n_init="auto").fit(norm_keypoints_list)
 
 cluster_center = kmeans.cluster_centers_
 labels = kmeans.labels_
+# cluster_center = get_cluster_center_by_choice(cluster_center, choice=[29, 26, 12, 27, 15, 7, 22, 5, 24, 28, 23, 13, 11, 20])
+# kmeans.cluster_centers_ = cluster_center
+# labels = kmeans.predict(np.array(norm_keypoints_list))
 
 write_cluster_centers(os.path.join(target_data_path, f'centers_30{tag}.txt'), cluster_center)
 write_pose_data(os.path.join(target_data_path, f'trainlist{tag}.txt'), image_name_list, keypoints_list, labels)
@@ -48,20 +36,7 @@ cluster_center_image = vis_clusters(image_size=(256, 256, 3), cluster_keypoints_
 cv2.imwrite(os.path.join(target_data_path, f'cluster_centers{tag}.jpg'), cluster_center_image)
 
 data_path = './affordance_data/testlist.txt'
-with open(data_path, 'r') as f:
-    data_list = list(f.readlines())
-data_list = [x.split(' ') for x in data_list]
-
-image_name_list = []
-keypoints_list = []
-norm_keypoints_list = []
-for data in data_list:
-    image_name = data[0]
-    keypoint = data[1:-3]
-    keypoint = [eval(x) for x in keypoint]
-    image_name_list.append(image_name)
-    keypoints_list.append(keypoint)
-    norm_keypoints_list.append(norm_pose(keypoint))
+image_name_list, keypoints_list, norm_keypoints_list = preprocess_keypoints_for_clustering(data_path)
 
 labels = kmeans.predict(np.array(norm_keypoints_list))
 write_pose_data(os.path.join(target_data_path, f'testlist{tag}.txt'), image_name_list, keypoints_list, labels)
